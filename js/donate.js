872d49925b0b86af2da4d5a5ee1072c098228a5c
ShoaibAlam107 <shoaib.cap107@gmail.com>
2024-09-24 16:11:24 +0600

    donate section complete with DOM

--- js/donate.js
+++ js/donate.js
@@ -1,1 +1,75 @@
-console.log('Remte is my love')
+
+
+document.getElementById('donate-for-noakhali')
+.addEventListener('click' , function(event){
+    event.preventDefault()
+    const inputMoney =getInputFieldValueById('input-amount-noakhali');
+    console.log('get input value',inputMoney);
+    const balance = getTextFieldById('donates-balance-noakhali');
+    const newDonateBalance = balance + inputMoney ;
+     document.getElementById('donates-balance-noakhali').innerText = newDonateBalance ;
+    const accountBalance = getTextFieldById('main-balance');
+    const availableBalance = accountBalance - newDonateBalance ;
+    document.getElementById('main-balance').innerText = availableBalance;
+    
+
+    // if(inputMoney === Number){
+      
+    // }
+    // else{
+    //     alert('wrong input')
+    // }
+
+});
+
+document.getElementById('donate-for-feni')
+.addEventListener('click' , function(event){
+    event.preventDefault()
+    const inputMoney =getInputFieldValueById('input-amount-feni');
+    console.log('get input value',inputMoney);
+    const balance = getTextFieldById('donates-balance-feni');
+    const newDonateBalance = balance + inputMoney ;
+     document.getElementById('donates-balance-feni').innerText = newDonateBalance ;
+    const accountBalance = getTextFieldById('main-balance');
+    const availableBalance = accountBalance - newDonateBalance ;
+    document.getElementById('main-balance').innerText = availableBalance;
+    
+
+    // if(inputMoney === Number){
+      
+    // }
+    // else{
+    //     alert('wrong input')
+    // }
+
+});
+
+
+document.getElementById('donate-for-student')
+.addEventListener('click' , function(event){
+    event.preventDefault()
+    const inputMoney =getInputFieldValueById('input-amount-student');
+    console.log('get input value',inputMoney);
+    const balance = getTextFieldById('donates-balance-student');
+    const newDonateBalance = balance + inputMoney ;
+     document.getElementById('donates-balance-student').innerText = newDonateBalance ;
+    const accountBalance = getTextFieldById('main-balance');
+    const availableBalance = accountBalance - newDonateBalance ;
+    document.getElementById('main-balance').innerText = availableBalance;
+    
+
+    // if(inputMoney === Number){
+      
+    // }
+    // else{
+    //     alert('wrong input')
+    // }
+
+});
+
+
+
+
+
+
+
---
+++ js/utilities.js
@@ -0,0 +1,14 @@
+
+
+function getInputFieldValueById(id){
+    
+    const inputValue = parseFloat (document.getElementById(id).value);
+    return inputValue;
+    
+}
+
+function getTextFieldById(id){
+    const textValue =document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
+}
